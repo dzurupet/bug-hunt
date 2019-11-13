004385c83c469ead995b6de039cc8673a3031ee5
Peter Dzuruš <dzurus.peter@gmail.com>
2019-11-13 13:08:53 +0100

    script change

--- static/script/chat.js
+++ static/script/chat.js
@@ -12,10 +12,15 @@
   a.async = 1
   a.src = g + '?' + Math.round(Date.now() / 1000 / 3600)
   m.parentNode.insertBefore(a, m)
-})(window, document, 'script', 'brandembassy', 'https://livechat-static.brandembassy.com/3/chat.js')
+})(
+  window,
+  document,
+  'script',
+  'brandembassy',
+  'https://livechat-static-qa.brandembassy.com/3/chat.js'
+)
 
-brandembassy('init', '849fb65bd9acc6e6a9f0')
-brandembassy('hidePopups')
+brandembassy('init', 6097, 'chat_1621706a-3516-4478-b398-ceff99c04bc6')
 brandembassy('setWindowWidth', '300px')
 brandembassy('setWindowHeight', '500px')
 brandembassy('setTranslation', 'replyBoxPlaceholder', 'Write a message')
